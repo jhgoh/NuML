7694f7a6efc4cb198de42cfba9129ce24ce52e49
Junghwan John Goh <jhgoh@cern.ch>
2019-05-04 03:22:18 +0900

    separate signal and bkg tree

--- FastSim/generatePseudoData.py
+++ FastSim/generatePseudoData.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from ROOT import *
 import sys, os
 import random
@@ -10,29 +12,24 @@ hSig = f.Get("hSig")
 hBkg1 = f.Get("hBkg1")
 
 fout = TFile("event.root", "recreate")
-tree = TTree("tree", "tree")
+treeS = TTree("treeS", "Signal tree")
+treeB = TTree("treeB", "Background tree")
 var = np.zeros((10,1), dtype=np.double)
 event = np.zeros((1,1), dtype=np.int32)
-category = np.zeros((1,1), dtype=np.int32)
-tree.Branch("event", event[0], "event/I")
-tree.Branch("catetory", category[0], "category/I")
-tree.Branch("En1", var[0], "En1/d")
-tree.Branch("En2", var[1], "En2/d")
-tree.Branch("dt", var[2], "dt/d")
-tree.Branch("dr", var[3], "dr/d")
-tree.Branch("x1", var[4], "x1/d")
-tree.Branch("y1", var[5], "y1/d")
-tree.Branch("z1", var[6], "z1/d")
-tree.Branch("x2", var[7], "x2/d")
-tree.Branch("y2", var[8], "y2/d")
-tree.Branch("z2", var[9], "z2/d")
-for i in range(10000):
+treeS.Branch("event", event[0], "event/I")
+treeB.Branch("event", event[0], "event/I")
+for i, varName in enumerate(["En1", "En2", "dt", "dr", "x1", "y1", "z1", "x2", "y2", "z2"]):
+    treeS.Branch(varName, var[i], varName+"/d")
+    treeB.Branch(varName, var[i], varName+"/d")
+
+for i in range(100000):
     event[0] = i
     if random.uniform(0,1) < sigFrac:
-        category[0] = 1
+        category = 1
         hSig.GetRandom(var)
+        treeS.Fill()
     else:
-        category[0] = 2
+        category = 2
         hBkg1.GetRandom(var)
-    tree.Fill()
+        treeB.Fill()
 fout.Write()
